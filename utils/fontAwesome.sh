a0a51d0cff346b58a5e5940bf3328cd404b6debc
Neil Martin <neilm@4js.com>
2023-06-23 12:07:50 +0100

    njmdemos_i2f

--- utils/fontAwesome.sh
+++ utils/fontAwesome.sh
@@ -12,6 +12,9 @@ cd $DIR
 #export FGLPROFILE=../etc/profile.ur
 export FGLRESOURCEPATH=../etc
 export FGLIMAGEPATH=$FGLDIR/lib/image2font.txt:$FGLDIR/lib
+if [ "$1" == "njm" ]; then
+	export FGLIMAGEPATH=../pics/njmdemos_i2f.txt:../pics
+fi
 if [ "$1" == "fa5" ]; then
 	export FGLIMAGEPATH=../pics/fa5.txt:../pics
 fi
